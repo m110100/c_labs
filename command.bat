49be510c8ebe90c703ed45275e9e4d76ba44854d
Истомин Артемий Всеволодович <stud143734@vyatsu.ru>
2024-12-26 23:05:55 +0300

    fix commands

--- command.bat
+++ command.bat
@@ -2,15 +2,13 @@
 
 if exist fib_static.o del fib_static.o
 if exist fib_static.lib del fib_static.lib
-if exist main.exe del main.exe
 if exist keyboard.o del keyboard.o
 if exist keyboard.dll del keyboard.dll
+if exist main.exe del main.exe
 
-g++ -c fib_static.cpp -o fib_static.o
-ar rcs fib_static.lib fib_static.o
-
-g++ main.cpp -o main.exe fib_static.lib
-
+g++ -c fib_static.cpp -DFIB_STATIC_EXPORTS -o fib_static.o
+g++ -shared -o fib_static.dll fib_static.o
 g++ -c keyboard.cpp -DKEYBOARD_EXPORTS -o keyboard.o
-
 g++ -shared -o keyboard.dll keyboard.o -luser32 -lpthread
+
+g++ main.cpp -o main.exe -L. -lfib_static
